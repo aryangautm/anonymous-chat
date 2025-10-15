ca336a744e6502daf178ea039344782d62857c11
aryangautam <aryan.gautam@roundglass.com>
2025-10-15 18:15:55 +0530

    Knowledge mutations and celery

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,5 +1,4 @@
 import uuid
-from datetime import datetime
 from sqlalchemy import Boolean, Column, DateTime, String
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
